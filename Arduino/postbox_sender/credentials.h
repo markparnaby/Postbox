b0f9ef313a8ccdae0416168dfeacb5b40d8750f1
Mark Parnaby <mark.parnaby@gmail.com>
2025-03-30 10:08:04 +0100

    First shot, with some debugs

---
+++ Arduino/postbox_sender/credentials.h
@@ -0,0 +1,3 @@
+#define WIFI_SSID "..."
+#define WIFI_PASS "..."
+#define SERVER "http://192.168.1.200:740/"
--- Arduino/postbox_sender/postbox_sender.ino
+++ Arduino/postbox_sender/postbox_sender.ino
@@ -1,9 +1,34 @@
-void setup() {
-  // put your setup code here, to run once:
+#include <WiFi.h>
+#include <HTTPClient.h>
+#include "credentials.h"
 
+void setup() {
+  Serial.begin(115200); 
+
+  WiFi.begin(WIFI_SSID, WIFI_PASS);
+  Serial.println("Connecting");
+  while(WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.print("Connected to WiFi network with IP Address: ");
+  Serial.println(WiFi.localIP());
+
+  
+  HTTPClient http;
+  String serverPath = SERVER + "api/postbox/state/full";
+
+  Serial.print("Sending the following as a POST request:");
+  Serial.println(serverPath.c_str());
+
+  http.begin(serverPath.c_str());
+  int httpResponseCode = http.POST();
+
+  
+  Serial.print("Response:");
+  Serial.println(httpResponseCode);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
 }
